f6001a0b2674063ba1f5701628e97fd08b876dd6
Kaio Matos <kaio_matos22@hotmail.com>
2023-04-10 19:30:32 -0300

    feat: add colors to outputs

--- philo/includes/headers.h
+++ philo/includes/headers.h
@@ -6,7 +6,7 @@
 /*   By: kmatos-s <kmatos-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 21:08:46 by kmatos-s          #+#    #+#             */
-/*   Updated: 2023/04/06 20:38:42 by kmatos-s         ###   ########.fr       */
+/*   Updated: 2023/04/10 19:28:07 by kmatos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,28 @@
 # include <pthread.h>
 # include <sys/time.h>
 # include <unistd.h>
+
+/* Black, Red, Green, Yellow, Blue, Purple, Cyan, White */
+# define SHELL_BK "\033[0;30m"
+# define SHELL_R "\033[0;31m"
+# define SHELL_G "\033[0;32m"
+# define SHELL_Y "\033[0;33m"
+# define SHELL_B "\033[0;34m"
+# define SHELL_P "\033[0;35m"
+# define SHELL_C "\033[0;36m"
+# define SHELL_W "\033[0;37m"
+
+/* Bold */
+# define SHELL_BKB "\033[1;30m"
+# define SHELL_RB "\033[1;31m"
+# define SHELL_GB "\033[1;32m"
+# define SHELL_YB "\033[1;33m"
+# define SHELL_BB "\033[1;34m"
+# define SHELL_PB "\033[1;35m"
+# define SHELL_CB "\033[1;36m"
+# define SHELL_WB "\033[1;37m"
+
+/* Reset Color */
+# define SHELL_RC "\033[0m"
 
 #endif
--- philo/log/log.c
+++ philo/log/log.c
@@ -6,7 +6,7 @@
 /*   By: kmatos-s <kmatos-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 20:23:15 by kmatos-s          #+#    #+#             */
-/*   Updated: 2023/04/06 21:16:30 by kmatos-s         ###   ########.fr       */
+/*   Updated: 2023/04/10 19:29:44 by kmatos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 void	log_taken_fork(t_philosopher *philosopher)
 {
-	printf("%ld\t %i has taken a fork\n",	get_program_time(), philosopher->id);
+	printf("%s%ld\t %i has taken a fork%s\n", SHELL_R, get_program_time(), philosopher->id, SHELL_RC);
 }
 
 void	log_eating(t_philosopher *philosopher)
 {
-	printf("%ld\t %i is eating\n", get_program_time(), philosopher->id);
+	printf("%s%ld\t %i is eating%s\n", SHELL_G, get_program_time(), philosopher->id, SHELL_RC);
 }
 
 void	log_sleeping(t_philosopher *philosopher)
 {
-	printf("%ld\t %i is sleeping\n", get_program_time(), philosopher->id);
+	printf("%s%ld\t %i is sleeping%s\n", SHELL_Y, get_program_time(), philosopher->id, SHELL_RC);
 }
 
 void	log_thinking(t_philosopher *philosopher)
 {
-	printf("%ld\t %i is thinking\n", get_program_time(), philosopher->id);
+	printf("%s%ld\t %i is thinking%s\n", SHELL_B, get_program_time(), philosopher->id, SHELL_RC);
 }
