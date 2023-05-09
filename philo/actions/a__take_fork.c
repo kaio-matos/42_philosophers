12e332e9247c0f2daa2f22afc11beb0300f5de4b
Kaio Matos <kaio_matos22@hotmail.com>
2023-05-08 21:22:59 -0300

    fix possible deadlock

--- philo/actions/a__take_fork.c
+++ philo/actions/a__take_fork.c
@@ -6,7 +6,7 @@
 /*   By: kmatos-s <kmatos-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 21:43:03 by kmatos-s          #+#    #+#             */
-/*   Updated: 2023/05/08 19:57:29 by kmatos-s         ###   ########.fr       */
+/*   Updated: 2023/05/08 21:20:11 by kmatos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,17 @@ t_fork	*a__take_fork(t_philosopher_routine *args, t_dlist *fork_node)
 
 	fork = get_fork(fork_node);
 	if (fork_node->next)
+	{
 		borrowed_fork = get_fork(fork_node->next);
-	else
+		pthread_mutex_lock(fork->mutex);
+		pthread_mutex_lock(borrowed_fork->mutex);
+	}
+	else if (get_fork(args->forks)->id != fork->id)
+	{
 		borrowed_fork = get_fork(args->forks);
-	pthread_mutex_lock(fork->mutex);
-	pthread_mutex_lock(borrowed_fork->mutex);
+		pthread_mutex_lock(borrowed_fork->mutex);
+		pthread_mutex_lock(fork->mutex);
+	}
 	if (!is_simulation_running(args->simulation))
 	{
 		a__put_forks_on_table(fork, borrowed_fork);
@@ -35,8 +41,6 @@ t_fork	*a__take_fork(t_philosopher_routine *args, t_dlist *fork_node)
 
 void	a__put_forks_on_table(t_fork *mine, t_fork *borrowed)
 {
-	if (mine)
-		pthread_mutex_unlock(mine->mutex);
-	if (borrowed)
-		pthread_mutex_unlock(borrowed->mutex);
+	pthread_mutex_unlock(borrowed->mutex);
+	pthread_mutex_unlock(mine->mutex);
 }
